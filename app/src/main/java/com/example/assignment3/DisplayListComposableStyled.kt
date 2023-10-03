5d63cb4a47069f51822174b8b56d375b7b347447
L-Filiatreault <laurenfiliatreault@gmail.com>
2023-10-02 23:34:01 -0400

    Updated the List
    Made each item appear prettier inside the item card now. Still working on the layout but it;s looking better

--- app/src/main/java/com/example/assignment3/DisplayListComposableStyled.kt
+++ app/src/main/java/com/example/assignment3/DisplayListComposableStyled.kt
@@ -4,10 +4,16 @@ package com.example.assignment3
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -16,6 +22,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 
@@ -45,14 +53,36 @@ fun DisplayListComposableStyled(item: PokemonGymInformation){
     )
     {
         //Aligning the text items inside to be more uniform-like
-        Row() {
+        Row(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+            )
+            {
+                val painter = rememberImagePainter(data = item.gymImageUrl)
+
+                Image(
+                    painter = painter,
+                    contentDescription = "A picture of a Pokemon gym",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
             Text(
                 text = "Gym Name: " + item.gymName,
                 modifier = Modifier.padding(4.dp),
-                style = MaterialTheme.typography.labelSmall, //Chose this font since it looks bold and stands out in a good way
+                style = MaterialTheme.typography.bodyMedium, //Chose this font since it looks bold and stands out in a good way
                 color = MaterialTheme.colorScheme.onPrimary
             )
-            BuildingTheCoilImage(item.gymImageUrl)
+
         }
     }
 }
