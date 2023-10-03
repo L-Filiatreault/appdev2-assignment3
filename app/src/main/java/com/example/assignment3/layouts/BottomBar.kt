a3181cc40b8c6f05e6c601e3a7796989b11546a0
L-Filiatreault <laurenfiliatreault@gmail.com>
2023-10-03 14:09:53 -0400

    Updated the Bottom Bar
    Able to make my Details page icon appear/disappear depending if the list is populated or not.

--- app/src/main/java/com/example/assignment3/layouts/BottomBar.kt
+++ app/src/main/java/com/example/assignment3/layouts/BottomBar.kt
@@ -1,5 +1,7 @@
 package com.example.assignment3.layouts
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.List
@@ -9,10 +11,13 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.assignment3.LocalNavController
+import com.example.assignment3.LocalPokemonList
 import com.example.assignment3.Routes
 
 data class NavBarIcon(
@@ -24,26 +29,42 @@ data class NavBarIcon(
 //Disable this until user has entered in some stuff
 
 
-val items = listOf(NavBarIcon(route= Routes.Main.route, icon= Icons.Outlined.Home),
-    NavBarIcon(route= Routes.ListScreen.route, icon= Icons.Outlined.List))
-    //NavBarIcon(route= Routes.Details.go("Pewter City", "Brock", "https://staticg.sportskeeda.com/editor/2021/03/ddff5-16153318599864.png"), icon= Icons.Outlined.Place))
 
 
 @Composable
 fun BottomBar()
 {
+    val pokemonListCurrent = LocalPokemonList.current
     val navController = LocalNavController.current
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
+
+
+    val items = mutableListOf<NavBarIcon>(NavBarIcon(route= Routes.Main.route, icon= Icons.Outlined.Home),
+        NavBarIcon(route= Routes.ListScreen.route, icon= Icons.Outlined.List))
+
+    //Adding the Details page Icon to show up between the Home and List screens, only if our list is populated
+    if(!pokemonListCurrent.isEmpty())
+    {
+         items.add(1,NavBarIcon(route= Routes.Details.go("0"), icon= Icons.Outlined.Place))
+    }
+
+
+
+
 
     NavigationBar {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
                 icon = { Icon(item.icon, contentDescription = item.route) },
+                label={item.route},
                 selected = currentDestination?.hierarchy?.any {
                     currentDestination.route?.substringBefore('/') ==
                             item.route.substringBefore('/') } == true,
-                onClick = { navController.navigate(item.route)}
+                onClick = { navController.navigate(item.route)},
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(24.dp)
             )
         }
     }
